6463e2ef5805d53ef63d61066ac369d541f19728
wangpengwei15 <wangpengwei15@jd.com>
2021-06-06 15:24:45 +0800

    init project

---
+++ src/main/java/com/aixohub/leetcode/AixohubLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.aixohub.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AixohubLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AixohubLeetcodeApplication.class, args);
+	}
+
+}
